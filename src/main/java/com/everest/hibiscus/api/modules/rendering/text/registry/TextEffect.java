00a8249851ba3aee8ab18b62ef2d8b31f829c4f7
thatoneaiguy <animations.by.alby@gmail.com>
2026-02-01 00:35:50 +0000

    update sig

--- src/main/java/com/everest/hibiscus/api/modules/rendering/text/registry/TextEffect.java
+++ src/main/java/com/everest/hibiscus/api/modules/rendering/text/registry/TextEffect.java
@@ -3,16 +3,10 @@ package com.everest.hibiscus.api.modules.rendering.text.registry;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.text.OrderedText;
 import net.minecraft.text.Style;
 import org.joml.Matrix4f;
 
 public interface TextEffect {
-    void render(DrawContext context, TextRenderer textRenderer, String text,
-                int x, int y, int color, Style style, float tickDelta);
-
-    void render(TextRenderer textRenderer, String text,
-                float x, float y, int color, boolean shadow,
-                Matrix4f matrix, VertexConsumerProvider vertexConsumers,
-                TextRenderer.TextLayerType layerType, int backgroundColor,
-                int light, Style style, float tickDelta);
+    void renderTooltipText(TextRenderer textRenderer, OrderedText text, int x, int y, int color, float tickDelta);
 }
